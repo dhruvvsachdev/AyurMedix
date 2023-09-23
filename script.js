dd97dc00fba94cce757c3824d1b82e9ba1c37c85
DivyamSaini25 <114746575+DivyamSaini25@users.noreply.github.com>
2023-09-24 01:34:39 +0530

    js.update

--- script.js
+++ script.js
@@ -2,36 +2,36 @@ var symptomChecker = [
     {
       symptoms: ['fatigue', 'fever', 'sore throat', 'diarrhea'],
       diagnosis: 'You are suffering from a common cold.',
-      cure: 'Rest,drink plenty of water, use saline nasal rinses.',
+      medicine: 'Rest,drink plenty of water, use saline nasal rinses.',
     },
     {
       symptoms: ['Fever', 'diarrhea','fatigue','Muscle aches'],
       diagnosis: 'You are suffering from a flu.',
-      cure: 'Rest and fluid intake.',
+      medicine: 'Rest and fluid intake.',
     },
     {
       symptoms: ['shortness of breath', 'chest pain', 'fatigue'],
       diagnosis: 'You are suffering from a heart condition.',
-      cure: 'consult a doctor',
+      medicine: 'consult a doctor',
     },
     {
       symptoms: ['High fever', 'rash','cough','stomach and muscle pain'],
       diagnosis: 'You are suffering from a Typhoid.',
-      cure: 'Antibiotics and fluid intake.',
+      medicine: 'Antibiotics and fluid intake.',
     },
     {
       symptoms: ['Itching','swelling'],
       diagnosis: 'You are suffering from a Fungal infection.',
-      cure: 'Antifungal  medication.',
+      medicine: 'Antifungal  medication.',
     },
     {
       symptoms: ['Itching','skin rash','facial swelling'],
       diagnosis: 'You are suffering from a Allergy.',
-      cure: 'Avoid allergens and steroids.',
+      medicine: 'Avoid allergens and steroids.',
     },{
       symptoms: ['Fever','headache','muscle aches','joint pain'],
       diagnosis: 'You are suffering from a AIDS.',
-      cure: 'Antiretroviral therapy.',
+      medicine: 'Antiretroviral therapy.',
     },
     // Add more symptom-diagnosis pairs as needed
   ];
@@ -39,7 +39,7 @@ var symptomChecker = [
   function checkSymptoms() {
     var symptoms = document.getElementById("symptoms").value.toLowerCase();
     var diagnosis = "No diagnosis found.";
-    var cure = "No cure found.";
+    var medicine = "No medicine found.";
   
     // Search for matching symptom set in the symptomChecker array
     for (var i = 0; i < symptomChecker.length; i++) {
@@ -57,12 +57,12 @@ var symptomChecker = [
       // If all symptoms are present, assign the corresponding diagnosis
       if (matched) {
         diagnosis = symptomChecker[i].diagnosis;
-        cure = symptomChecker[i].cure;
+        medicine = symptomChecker[i].medicine;
 
         break;
       }
     }
   
     document.getElementById("resultText").innerHTML = '<p>'+diagnosis + '</p>';
-    document.getElementById("resultText1").innerHTML = '<p>'+cure + '</p>';
+    document.getElementById("resultText1").innerHTML = '<p>'+medicine + '</p>';
   }
